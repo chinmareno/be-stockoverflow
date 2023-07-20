800bad775bb8d860caa22a1bf83657a5afc31d31
iyah <chinmareno1@gmail.com>
2023-07-20 13:34:23 +0700

    add header response

--- app.ts
+++ app.ts
@@ -57,6 +57,13 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use("/images", express.static(path.join(__dirname, "public")));
